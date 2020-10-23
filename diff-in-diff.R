aff1ffee657ee7292474c4ff33f303ed18bd853e
Mario A. Garcia-Meza <marius.agm@gmail.com>
2020-10-23 10:51:42 -0500

    Creamos la grafica con el contrafactual

---
+++ diff-in-diff.R
@@ -0,0 +1,43 @@
+# Usanndo Diferencias en Diferencias 
+
+# Cargamos la base de datos
+
+banks <- read.csv("data/banks.csv")
+attach(banks)
+
+str(banks)
+
+banks$date <- ISOdate(year, month, day)
+
+head(banks)
+
+banks$lbib6 <- log(bib6)
+banks$lbio6 <- log(bio6)
+banks$lbib8 <- log(bib8)
+banks$lbio8 <- log(bio8)
+
+attach(banks)
+
+plot(date, lbio6, type = "l", col = "blue", lwd = "3")
+lines(date, lbio8, col = "red", lwd = "3")
+
+
+plot(date, lbio6, type = "l", col = "blue", lwd = "3")
+lines(date, lbio8, col = "red", lwd = "3")
+
+
+# Crear contrafactual
+
+banks <- banks[month == 7 & day == 1,]
+diff <- bib8 - bib6
+bibc <- bib6 * (year == 1929) + (bib8 - diff[2]) * (year >= 1930)
+
+ldiff <- lbib8 - lbib6
+lbibc <- lbib6 * (year == 1929) + (lbib8 - ldiff[2]) * (year >= 1930)
+
+# plot levels
+attach(banks)
+plot(date, bib8, type = "l", col = "red", ylim = c(80,170))
+lines(date, bib6, col = "blue")
+lines(date, bibc, col = "blue", lty = "dashed")
+
