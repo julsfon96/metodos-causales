b04d8d980b9bdd2e9365597614cb254442309364
Mario García Meza <marius.agm@gmail.com>
2020-10-24 21:04:59 -0500

    modelo dd

--- diff-in-diff.R
+++ diff-in-diff.R
@@ -18,8 +18,8 @@ banks$lbio8 <- log(bio8)
 
 attach(banks)
 
-plot(date, lbio6, type = "l", col = "blue", lwd = "3")
-lines(date, lbio8, col = "red", lwd = "3")
+plot(date, lbib6, type = "l", col = "blue", lwd = "3")
+lines(date, lbib8, col = "red", lwd = "3")
 
 
 plot(date, lbio6, type = "l", col = "blue", lwd = "3")
@@ -41,3 +41,24 @@ plot(date, bib8, type = "l", col = "red", ylim = c(80,170))
 lines(date, bib6, col = "blue")
 lines(date, bibc, col = "blue", lty = "dashed")
 
+######################################
+
+dd <- (bib6[match(1931,year)] - bib6[match(1930,year)]) - (bib8[match(1931,year)] - bib8[match(1930,year)])
+dd
+
+dd <- (bib6[match(1931,year)] - bib8[match(1931,year)]) - (bib6[match(1930,year)] - bib8[match(1930,year)])
+dd
+
+
+#########################################
+banksLong <- reshape2::melt(banks[c(1,6:13)], id.vars = "date")
+
+banksLong$bib6 <- ifelse(banksLong$variable == "bib6",1,0)
+
+bnk <- banksLong[banksLong$variable %in% c("bib6", "bib8"),]
+bnk$post <- ifelse(bnk$date >= "1931-07-01",1,0)
+
+attach(bnk)
+
+DDMi <- lm(value ~ bib6 + post + bib6 * post)
+summary(DDMi)
